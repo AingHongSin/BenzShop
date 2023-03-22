8a1a8a908894093cb7d12ad74a7199c0f0f965e6
Hongsin <ainghongsin9999@gmail.com>
2023-03-22 15:41:30 +0700

    Finish Home page

--- BenzShop/src/App.vue
+++ BenzShop/src/App.vue
@@ -23,6 +23,8 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
 header {
   line-height: 1.5;
   max-height: 100vh;
